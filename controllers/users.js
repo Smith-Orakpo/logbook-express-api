905b8f919ae257330f903e41904406f2569f773a
Smith Orakpo <grapevinecode@gmail.com>
2020-04-20 03:31:53 -0700

    write users.login controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,24 @@
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 const { User } = require("../database")
 
-const login = (req, res, next) => {}
+const login = async (req, res, next) => {
+	const { email, password } = req.body
+	try {
+		const user = await User.findOne({ where: { email } })
+		if (!user) return res.status(400).send("invalid credentials")
+
+		const match = await bcrypt.compare(password, user.password)
+		if (!match) return res.status(400).send("invalid credentials")
+
+		const token = await jwt.sign({ user }, process.env.SECRET, {
+			expiresIn: "1h"
+		})
+		res.send(token)
+	} catch (error) {
+		next(error)
+	}
+}
 
 const signup = (req, res, next) => {}
 
