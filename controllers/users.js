42e461df0dd43d11b360c68f646bf6f444fae654
Smith Orakpo <grapevinecode@gmail.com>
2020-04-20 03:34:55 -0700

    write users.signup controller

--- controllers/users.js
+++ controllers/users.js
@@ -20,7 +20,24 @@ const login = async (req, res, next) => {
 	}
 }
 
-const signup = (req, res, next) => {}
+const signup = async (req, res, next) => {
+	const { email, password } = req.body
+	try {
+		let user = await User.findOne({ where: { email } })
+		if (user) return res.status(400).send("registered")
+
+		const SALT = await bcrypt.genSalt(10)
+		const HASH = await bcrypt.hash(password, SALT)
+
+		user = await User.create({ email, password: HASH })
+		const token = await jwt.sign({ user }, process.env.SECRET, {
+			expiresIn: "1h"
+		})
+		res.send(token)
+	} catch (error) {
+		next(error)
+	}
+}
 
 module.exports = {
 	login,
