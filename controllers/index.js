2a5821e1aa3a8666720536f8a1dea8d2b95cb24c
Smith Orakpo <grapevinecode@gmail.com>
2020-04-20 02:50:38 -0700

    scaffold logs controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,7 @@
+const logs = require("./logs")
 const users = require("./users")
 
 module.exports = {
+	logs,
 	users
 }
---
+++ controllers/logs.js
@@ -0,0 +1,17 @@
+const all = (req, res, next) => {}
+
+const one = (req, res, next) => {}
+
+const add = (req, res, next) => {}
+
+const upd = (req, res, next) => {}
+
+const rmv = (req, res, next) => {}
+
+module.exports = {
+	all,
+	one,
+	add,
+	upd,
+	rmv
+}
