695116931e59079a13ba55a4a044452d0894d97b
Smith Orakpo <grapevinecode@gmail.com>
2020-04-20 03:13:06 -0700

    integrate utils with router

--- router/index.js
+++ router/index.js
@@ -1,15 +1,20 @@
 const router = require("express").Router()
 const { logs, users } = require("../controllers")
+const { auth, data } = require("../utils")
 
 router.get("/", (req, res) => res.send("logbook"))
 
-router.route("/logs").get(logs.all).post(logs.add)
+router.route("/logs").get(auth, logs.all).post(auth, logs.add)
 
-router.route("/logs/:id").get(logs.one).put(logs.upd).delete(logs.rmv)
+router
+	.route("/logs/:id")
+	.get(auth, logs.one)
+	.put(auth, logs.upd)
+	.delete(auth, logs.rmv)
 
-router.post("/users/login", users.login)
+router.post("/users/login", data, users.login)
 
-router.post("/users/signup", users.signup)
+router.post("/users/signup", data, users.signup)
 
 router.use((req, res) => res.status(400).send("not found"))
 
